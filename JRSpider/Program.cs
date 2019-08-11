d98d0623c39e6a371491d7342b538486ec8e9774
angryshoes <284310681@qq.com>
2019-08-11 14:32:08 +0800

    reorgnize folder and configuration

---
+++ JRSpider/Program.cs
@@ -0,0 +1,23 @@
+﻿using JRSpider.Models;
+using System;
+using System.IO;
+using System.Net;
+
+namespace JRSpider
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            if (!Directory.Exists(ConfigModel.SavePath))
+            {
+                Console.WriteLine("Please Enter a valid save path...");
+                ConfigModel.SavePath = Console.ReadLine();
+            }
+            JRSpiderHelper.DownloadImages(ConfigModel.Url, ConfigModel.SavePath, webClient);
+            webClient.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
